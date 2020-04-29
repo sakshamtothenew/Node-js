5c8c2bd75fa9f558e6b721cc10bff8e323be73d9
saksham <saksham.sachdeva@tothenew.com>
2020-04-29 17:37:56 +0530

    assignment2

---
+++ session2/src/components/Students/Students.js
@@ -0,0 +1,71 @@
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
+import classes from './student.module.css'
+
+
+const Student = () => {
+    const [studentData, setStudentData] = useState([]);
+    useEffect(() => {
+
+        axios.get('http://localhost:4000/')
+            .then((Response) => {
+                setStudentData(Response.data);
+            })
+
+    }, [])
+
+
+    const branchchanghandler = (event) => {
+        console.log(event.target.value)
+        const branch = event.target.value
+        const query = `?branch=${branch}`
+        axios.get('http://localhost:4000/students' + query)
+            .then(Response => {
+                console.log("state.chane")
+                setStudentData(Response.data)
+            })
+    }
+
+    const students = studentData.map((eachStudent) => {
+
+        return (
+            <tr>
+                <td>{eachStudent.firstname}</td>
+                <td>{eachStudent.lastname}</td>
+                <td>{eachStudent.branch}</td>
+            </tr>
+        )
+    })
+
+    return (
+        <div className = {classes.Ques2}>
+            <h1>this is the students page</h1>
+            <div>
+            <label>filter branch</label>
+            <select className={classes.Filter} onChange={branchchanghandler}>
+                <option>ALL</option>
+                <option>electronics</option>
+                <option>CSE</option>
+                <option>mechanical</option>
+            </select>
+            <table className = {classes.StudentTable}>
+                <thead>
+                    <tr>
+                        <th>firstname</th>
+                        <th>lastname</th>
+                        <th>branch</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {students}
+                </tbody>
+
+            </table>
+            </div>
+          
+        </div>
+    )
+}
+
+
+export default Student
